703c2bf8c1c5758841675202c1f269654e6e114a
blackbaud-brandonhare <brandon.hare@blackbaud.com>
2017-05-03 13:48:46 -0400

    Added tests for plugin

--- src/include.js
+++ src/include.js
@@ -1,6 +1,7 @@
 const cheerio = require('cheerio');
 const fs = require('fs');
-const directory = './assets/includes/'
+const path = require('path');
+const root = path.resolve(process.cwd(), 'src', 'stache', 'includes');
 
 const preload = (content, resourcePath) => {
   if (!resourcePath.match(/\.html$/)) {
@@ -13,14 +14,14 @@ const preload = (content, resourcePath) => {
     decodeEntities: false
   });
 
-  let includes = $('stache-includes');
+  let includes = $('stache-include');
 
   if (includes.length) {
     includes.each(function () {
-      let fileName = directory + $(this).attr('fileName');
-      let file = fs.readFile(fileName, (err, data) => {
-        $(this).replaceWith(data);
-      });
+      let fileName = $(this).attr('fileName');
+      let filePath = path.join(root, fileName);
+      let file = $(fs.readFileSync(filePath, 'utf8'));
+      $(this).text(file);
     });
 
     content = $.html().toString();
